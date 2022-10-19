ca5cdc69a489c5093a0f902d89684be851a9ac2c
Jack Jutzi <jsjutzi@gmail.com>
2022-10-19 15:42:51 -0400

    add first integration test

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            // web::get() is shorthand for Route::new().guard(guard: Get())
-            // ie -> give request to handler if and only if it's method is GET
-            .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+  run()?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,24 @@
+#[tokio::test]
+
+async fn health_check_works() {
+    // Arrange
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}
