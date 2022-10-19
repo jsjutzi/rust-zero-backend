4bab604ae64fb45948457f8fd75a33d241a33f83
Jack Jutzi <jsjutzi@gmail.com>
2022-10-19 11:14:34 -0400

    upgrade to nightly rust and add health check

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -9,6 +13,9 @@ async fn greet(req: HttpRequest) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            // web::get() is shorthand for Route::new().guard(guard: Get())
+            // ie -> give request to handler if and only if it's method is GET
+            .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
