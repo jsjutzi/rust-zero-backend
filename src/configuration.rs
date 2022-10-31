d3201fce8dff3ffdf7dbf0d968c1167b5e807899
Jack Jutzi <jsjutzi@gmail.com>
2022-10-31 15:57:16 -0400

    add config file and database settings

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,2 +1,29 @@
 #[derive(serde::Deserialize)] 
-pub struct Settings {}
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Initialize configuration reader
+    let settings = config::Config::builder()
+        // Add configuration from file 'configuration.yaml'
+        .add_source(
+            config::File::new("configuration.yaml", config::FileFormat::Yaml)
+        )
+        .build()?;
+
+    // Try to convert configuration values it read into
+    // our Settings type
+    settings.try_deserialize::<Settings>()
+}
+
