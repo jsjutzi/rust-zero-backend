079245e2a86893dbb7b5ea7f740bb9480d567643
Jack Jutzi <jsjutzi@gmail.com>
2022-11-13 11:48:21 -0500

    complete migration from Log to Tracing

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,10 @@
 use crate::routes::{health_check, subscribe};
 
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // We have to wrap connection in an Arc<T> so that every new worker
@@ -12,7 +12,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
