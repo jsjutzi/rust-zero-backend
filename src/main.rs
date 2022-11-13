1e99d2e6f414f08f7aefc9c7c9731f80ca3ae592
Jack Jutzi <jsjutzi@gmail.com>
2022-11-13 15:04:10 -0500

    fix clippy linting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use secrecy::ExposeSecret;
-use sqlx::{postgres::PgPoolOptions, PgPool};
+use sqlx::{postgres::PgPoolOptions};
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
+#[allow(clippy::needless-borrow)]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
