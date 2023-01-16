a35a06400f653050b653a2af4b2e6df5228e1f5a
Jack Jutzi <jsjutzi@gmail.com>
2023-01-16 12:05:30 -0500

    formatting

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -16,7 +16,6 @@ async fn an_error_flash_message_is_set_on_failure() {
     // Act Try logging in
     assert_is_redirect_to(&response, "/login");
 
-
     // Act Part 2 - Following redirect
     let html_page = app.get_login_html().await;
     assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
